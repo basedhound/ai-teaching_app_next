c35ee0bce3c7667a9f5601414ea2a458efadce96
Jozef S <franck.vukelic@gmail.com>
2025-05-15 14:13:35 +0700

    Add Popular Companions section to Homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,44 @@
-const Homepage = () => {
-  return (
-    <div>Homepage</div>
-  )
-}
+import CTA from "@/components/CTA";
 
-export default Homepage
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+
+export default async function Home() {
+  return (
+    <main>
+      <h1>Popular Companions</h1>
+
+      <section className="home-section">
+        <CompanionCard
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="science"
+          duration={30}
+          color="#E5D0FF"
+        />
+        <CompanionCard
+          id="456"
+          name="Countsy the Number Wizard"
+          topic="Derivatives & Integrals"
+          subject="maths"
+          duration={45}
+          color="#FFDA6E"
+        />
+        <CompanionCard
+          id="789"
+          name="Verba the Vocabulary Builder"
+          topic="language"
+          subject="English Literature"
+          duration={30}
+          color="#BDE7FF"
+        />
+      </section>
+
+      <section className="home-section">
+        <CompanionsList />
+        <CTA />
+      </section>
+    </main>
+  );
+}
