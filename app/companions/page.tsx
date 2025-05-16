ff106acddd0ea479c1f64a2d9f0d6ea38cb72f9c
Jozef S <franck.vukelic@gmail.com>
2025-05-16 16:29:27 +0700

    Update Copanion Library

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import { getSubjectColor } from "@/lib/utils";
+import { getAllCompanions } from "@/lib/actions/companion.actions";
+import CompanionCard from "@/components/CompanionCard";
+import SearchInput from "@/components/SearchInput";
+import SubjectFilter from "@/components/SubjectFilter";
 
-const CompanionsPage = () => {
+
+const CompanionsPage = async ({ searchParams }: SearchParams) => {
+  const filters = await searchParams;
+  const subject = filters.subject ? filters.subject : "";
+  const topic = filters.topic ? filters.topic : "";
+
+  const companions = await getAllCompanions({ subject, topic });
+
   return (
-    <div>CompanionsPage</div>
-  )
-}
+    <main>
+      <section className="flex justify-between gap-4 max-sm:flex-col">
+        <h1>Companion Library</h1>
+        <div className="flex gap-4">
+          <SearchInput />
+          <SubjectFilter />
+        </div>
+      </section>
+      <section className="companions-grid">
+        {companions.map((companion) => (
+          <CompanionCard
+            key={companion.id}
+            id={companion.id}
+            name={companion.name}
+            subject={companion.subject}
+            topic={companion.topic}
+            duration={companion.duration}
+            color={getSubjectColor(companion.subject)}
+          />
+        ))}
+      </section>
+    </main>
+  );
+};
 
-export default CompanionsPage
+export default CompanionsPage;
