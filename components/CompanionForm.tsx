95d390e3b869b6325670d71025c75584a89ccc81
Jozef S <franck.vukelic@gmail.com>
2025-05-23 17:22:43 +0700

    Fix deployment error

--- components/CompanionForm.tsx
+++ components/CompanionForm.tsx
@@ -23,7 +23,7 @@ import {
 } from "@/components/ui/select";
 import { subjects } from "@/constants";
 import { Textarea } from "./ui/textarea";
-import { createCompanion } from "@/lib/actions/companion.actions";
+import {createCompanion} from "@/lib/actions/companion.actions";
 import { redirect } from "next/navigation";
 
 const FormSchema = z.object({
